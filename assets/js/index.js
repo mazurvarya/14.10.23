67f6c03710e359856763fd156cd73ccae1bd3bef
barbara3 <130148821+mazurvarya@users.noreply.github.com>
2023-12-09 18:26:34 +1000

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+let name = "Barbara"
+console.log(name);
+
+var name1 = "Barbara var"
+console.log(name1);
+
+const name_const = "Barbara const"
+console.log(name_const);
+
+    if (1==1){
+ } else {
+
+ }
+
+ for (let i = 0; i < 10; i++) {
+    console.log(i); 
+ }
+
+ let a = 10
+ while (a > 0) {
+    console.log(a);
+    a--;
+ }
+
+function Funcname(params) {
+    let Letname = "let";
+    var Varname = "var";
+
+    console.log(Letname, Varname);
+}
+Funcname();
+
