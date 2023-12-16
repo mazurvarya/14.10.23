693187ab55c79313fa8e5c0c0a4abb17df472b85
barbara3 <130148821+mazurvarya@users.noreply.github.com>
2023-12-16 18:26:33 +1000

    Add files via upload
    js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,50 @@
-let name = "Barbara"
-console.log(name);
+// let name = "Barbara"
+// console.log(name);
 
-var name1 = "Barbara var"
-console.log(name1);
+// var name1 = "Barbara var"
+// console.log(name1);
 
-const name_const = "Barbara const"
-console.log(name_const);
+// const name_const = "Barbara const"
+// console.log(name_const);
 
-    if (1==1){
- } else {
+//     if (1==1){
+//  } else {
 
- }
+//  }
 
- for (let i = 0; i < 10; i++) {
-    console.log(i); 
- }
+//  for (let i = 0; i < 10; i++) {
+//     console.log(i); 
+//  }
 
- let a = 10
- while (a > 0) {
-    console.log(a);
-    a--;
- }
+//  let a = 10
+//  while (a > 0) {
+//     console.log(a);
+//     a--;
+//  }
 
-function Funcname(params) {
-    let Letname = "let";
-    var Varname = "var";
+// function Funcname(params) {
+//     let Letname = "let";
+//     var Varname = "var";
 
-    console.log(Letname, Varname);
-}
-Funcname();
+//     console.log(Letname, Varname);
+// }
+// Funcname();
 
+let title = document.getElementsByClassName("header-title");
+console.log("все элементы с тэгом h1", title);
+
+let title2 = document.getElementsByTagName("h2")
+console.log('все элементы с тэгом h2', title2)
+
+let switchBtn = document.getElementById("switch")
+console.log("переключатель", switchBtn)
+
+let navigation = document.querySelectorAll(".navigation-menu h3")
+console.log("navigation")
+
+let navitem = document.querySelector(".navigation-menu h3")
+console.log(navitem)
+
+navitem.innerHTML = "Новый текст"
+navitem.style.color = "black"
+navitem.style.borderBottom = "2px solid black"
